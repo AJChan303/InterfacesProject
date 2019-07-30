e1141d0febe3af254615bf5349fe64e28c56fe59
Alex Chan <AJChan303@gmail.com>
2019-07-30 13:43:42 -0400

    completed

--- InterfacesProject/Program.cs
+++ InterfacesProject/Program.cs
@@ -3,7 +3,31 @@
 namespace InterfacesProject {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            IStudent greg = new FakeGreg();
+            Student[] students = greg.GetStudents();
+            int index = 0;
+            while (index < students.Length) {
+                Console.WriteLine($"Student name is {students[index].Name}");
+                index = index + 1;
+            }
+
+        }
+    }
+    class Student {
+    public string Name { get; set; }
+    }
+    interface IStudent {
+
+        Student[] GetStudents();
+    }
+    class FakeGreg: IStudent {
+        public Student[] GetStudents() {
+            Student[] students = new Student[] {
+                new Student{ Name = "Denise" },
+                new Student{ Name = "Kim" },
+                new Student{ Name = "Greg" }
+            };
+            return students;
         }
     }
 }
